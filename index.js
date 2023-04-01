c0c022609a6e6cd40faf912039c4248128f4beb1
JacobJershin <70407880+JacobJershin@users.noreply.github.com>
2023-04-01 13:15:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ app.set("view engine", "ejs");
 app.engine('ejs', require('ejs').__express);
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/v1/level/placement/:filename', async (req, res) => {
+app.use('/DecodeAPI/v1/demons/id/:filename', async (req, res) => {
+    console.log("loading query");
+    const placement = req.params.filename.toString()
+
+    const url = "https://script.google.com/macros/s/AKfycbwFlOSzcttbpNSilFl3V7RtpYtlllZ_wUfspjLWMXRJzVo49ChNeuRM8xo_IA69DXU4fg/exec?action=getData";
+    const response = await fetch(url);
+    var JSON = await response.json();
+
+    res.send(JSON[parseInt(placement)]);
+});
+
+app.use('/DecodeAPI/v1/demons/placement/:filename', async (req, res) => {
+    console.log("loading query");
+    const placement = req.params.filename.toString()
+
+    const url = "https://script.google.com/macros/s/AKfycbwFlOSzcttbpNSilFl3V7RtpYtlllZ_wUfspjLWMXRJzVo49ChNeuRM8xo_IA69DXU4fg/exec?action=getData";
+    const response = await fetch(url);
+    var JSON = await response.json();
+
+    res.send(JSON[parseInt(placement)]);
+});
+
+
+app.use('/Decode/v1/demons/listed/', async (req, res) => {
     console.log("loading query");
     const placement = req.params.filename.toString()
 
@@ -19,5 +42,19 @@ app.use('/v1/level/placement/:filename', async (req, res) => {
 
     res.send(JSON[parseInt(placement)]);
 });
+
+/*
+app.use('/v1/POST/level/:filename', async (req, res) => {
+    console.log("loading query");
+    const placement = req.params.filename.toString()
+
+    const url = "https://script.google.com/macros/s/AKfycbx0sxjsBIKdZwktRzb7goz6ThKVBKmCzYLCrC83RHJonbvqYqe_CG9R_jIf7sOs7FW3hQ/exec?action=getData";
+    const response = await fetch(url);
+    var JSON = await response.json();
+    console.log(JSON);
+
+    res.send(JSON[parseInt(placement)]);
+});
+*/
 
 app.listen(3000, (req, res) => console.log("running on 3000"));
